8b368fe67fc571141cda08eeb71a9700a2870c1f
Alistair Curd <a.curd@leeds.ac.uk>
2026-01-30 17:49:08 +0000

    Try autodoc2 and myST.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -5,32 +5,37 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src/perpl'))  # Adjust to include your package
+sys.path.insert(0, os.path.abspath("../../src/perpl"))  # Adjust to include your package
 
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'PERPL'
-copyright = '2025, Alistair Curd'
-author = 'Alistair Curd'
+project = "PERPL"
+copyright = "2025, Alistair Curd"
+author = "Alistair Curd"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.intersphinx',
+    # "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.intersphinx",
     "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
-    'sphinx_rtd_theme',
+    "sphinx_rtd_theme",
+    "myST_parser"
+    "autodoc2"
 ]
 
+autodoc2_packages = [
+    "../../src/perpl"
+]
 pygments_style = "sphinx"       # enable syntax highlighting
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 # Napoleon settings for Google/NumPy-style docstrings
@@ -40,5 +45,5 @@ napoleon_numpy_docstring = True
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
